b4463b0fe65ec1db56f22980aaa6ad3175b91f12
jhonlynx <jpaulnoquiana2@gmail.com>
2025-05-07 15:19:49 +0800

    Your commit message describing the changes

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', \App\Models\example\User::class),
+            'model' => env('AUTH_MODEL', \App\Models\Dentist::class),
         ],
 
         // 'users' => [

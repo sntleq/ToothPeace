6b3fe036fbdb8b3b09dcf11e22945398ec67029c
Jonathan Mangallay <mangallay.jonathan@gmail.com>
2025-05-21 21:10:23 +0800

    Patient Dynamic 2.0

--- routes/dentist.php
+++ routes/dentist.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Appointment;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Crud\AvailabilityController;
 use App\Http\Controllers\Crud\AvailabilityOverrideController;
@@ -28,11 +29,15 @@ Route::middleware('auth:dentist')
             ->name('availability.override.update');
 
         Route::get('/schedule', function () {
-            return view('dentist_schedule');
+            $appointments = Appointment::with(['patient', 'appointmentType'])->get();
+            return view('dentist_schedule', compact('appointments'));
         })->name('schedule');
 
         Route::get('/schedule/history', function () {
-            return view('dentist_schedule_history');
+            $appointments = Appointment::with(['patient', 'appointmentType'])
+                ->where('is_active', false)
+                ->get();
+            return view('dentist_schedule_history', compact('appointments'));
         })->name('schedule.history');
 
     });
